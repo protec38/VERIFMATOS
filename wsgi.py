f74567049535e46d85a0629ee1b20314b5a9a6ab
Lucas Pecout <100285952+lucaspecout@users.noreply.github.com>
2025-09-23 13:30:56 +0200

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,11 +1,4 @@
-
 from app import create_app
 
-# Gunicorn entrypoint: gunicorn 'wsgi:create_app()' --bind 0.0.0.0:8000
-def create_app():
-    return _create()
-
-def _create():
-    # Keep a separate function name so unit tests can import it without executing gunicorn-specific code.
-    from app import create_app as factory
-    return factory()
+# Gunicorn cherchera "app" ici
+app = create_app()
